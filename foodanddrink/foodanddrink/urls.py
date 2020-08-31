c09b71eeb86a07bfff108406a1f05c714660b974
HienDao <hiendao.kt21@gmail.com>
2020-08-31 11:29:45 +0700

    Fix multilanguage

--- foodanddrink/foodanddrink/urls.py
+++ foodanddrink/foodanddrink/urls.py
@@ -19,8 +19,16 @@ from django.views.generic.base import TemplateView
 from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.conf.urls.static import static
+from django.utils.translation import gettext_lazy as _
+from restaurant.views import change_language
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
+    path('change_language/',
+         change_language,
+         name='change_language')
+]
+
+urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('restaurant.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
--- foodanddrink/restaurant/views.py
+++ foodanddrink/restaurant/views.py
@@ -28,10 +28,27 @@ from django.db.models import Avg
 
 from django.views.generic import ListView
 from django.db.models import Q
+from django.conf import settings
 
 
 UserModel = get_user_model()
 DEFAULT_AVATAR = 'media/profile_pics/default.jpg'
+def change_language(request):
+  response = HttpResponseRedirect('/')
+  if request.method == 'POST':
+      language = request.POST.get('language')
+      if language:
+          if language != settings.LANGUAGE_CODE and [lang for lang in settings.LANGUAGES if lang[0] == language]:
+              redirect_path = f'/{language}/'
+          elif language == settings.LANGUAGE_CODE:
+              redirect_path = '/'
+          else:
+              return response
+          from django.utils import translation
+          translation.activate(language)
+          response = HttpResponseRedirect(redirect_path)
+          response.set_cookie(settings.LANGUAGE_COOKIE_NAME, language)
+  return response
 
 def index(request):
     list_category = Category.objects.all()
